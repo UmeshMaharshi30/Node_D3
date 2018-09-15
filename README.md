8f20a9c0a04da405a55981d0d14416ee81261212
UmeshMaharshi30 <you@example.com>
2018-09-14 20:05:14 -0500

    added read me and comments for better understanding of the code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Node_D3
+
+This project is an example on how to use jsdom and d3.
+
+## Getting Started
+
+
+### Prerequisites
+
+
+### Installing
+
+1. Clone the project
+2. npm install
+
+
+## Running the tests
+// use this to run
+node index.js
+
+On successful run it should create files "output.pdf" and "output.svg". 
+"output.svg" file contains the svg of the data viz
+"output.pdf" file contains the pdf with svg in it
+
+## Authors
+
+* **Umesh Maharshi** - *Initial work* - [UmeshMaharshi30](https://github.com/UmeshMaharshi30)
+
+See also the list of [contributors](https://github.com/UmeshMaharshi30/Node_D3/graphs/contributors) who participated in this project.
+
+
+## Acknowledgments
+
+* D3
+* Pdfkit
+* jsdom
+* Read me template [PurpleBooth](https://github.com/PurpleBooth)
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Instructions:
-//  npm install --save d3 jsdom
-
 
 const fs = require('fs');
 const d3 = require('d3');
@@ -8,8 +5,6 @@ const o = require("./lib/outreach.js");
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 var svgtopdf = require('svg-to-pdfkit');
-var http = require('http');
-var url = require('url') ;
 
 var csv = require('csv-parser');
 var kit = require('pdfkit');
@@ -25,51 +20,45 @@ const fakeDom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
 const outputLocation = './output.svg';
 const graphBackGround = "#F0F0F0";
 var apples = o.generateObject();
-                apples.title = "Apple Market Details";  
-                apples.xLabel = "year";
-                apples.xKey = "Year";
-                apples.yLabel = "Production lbs";
-                apples.yKey = "Value";
-                apples.dataUrl = "data/apples.csv";
-                apples.dataType = "csv";
-                apples.data = null;
-                apples.containerId = "apples";
-                apples.graphDimentions = [700, 600];  
-                //o.multilineChart(apples);
+    apples.title = "Apple Market Details";  // graph title
+    apples.xLabel = "year"; // x axis label
+    apples.xKey = "Year"; 
+    apples.yLabel = "Production lbs"; // y axis label
+    apples.yKey = "Value";
+    apples.dataUrl = "data/apples.csv"; // path of the data file relative to the file in which it is being called 
+    apples.dataType = "csv";
+    apples.data = null; 
+    apples.containerId = "apples";
+    apples.graphDimentions = [700, 600]; // dimensions of the graph  
+    //o.multilineChart(apples);
 
+// creates an empty html document    
 let body = d3.select(fakeDom.window.document).select('body');
 
 // Make an SVG Container
 let svgContainer = body.append("svg").attr("id", apples.containerId).style("width", apples.graphDimentions[0] + "px").style("height", apples.graphDimentions[1] + "px").style("background-color", graphBackGround);
-                /*
-                fs.createReadStream("./" + apples.dataUrl)
-                .pipe(csv())
-                .on('data', function (data) {
-                    
-                });
-                */
-                var contents = fs.readFileSync("./" + apples.dataUrl, 'utf8');
-                apples.rawData = d3.csvParse(contents);
-                o.multilineChart(apples, svgContainer);
-                //o.groupedSTackBar(apples, svgContainer);
-                var doc = new kit();
-                doc.pipe(fs.createWriteStream('output.pdf'));
-                svgtopdf(doc, body.html(), 20, 20);
-                doc.end();
-                fs.writeFileSync(outputLocation, body.html());                
 
+// reads the data file, has to be a csv  
+var contents = fs.readFileSync("./" + apples.dataUrl, 'utf8');
 
-// Draw a line
-/*
-let circle = svgContainer.append("line")
-  .attr("x1", 5)
-  .attr("y1", 5)
-  .attr("x2", 500)
-  .attr("y2", 500)
-  .attr("stroke-width", 2)
-  .attr("stroke", "black");
-*/
-// Output the result to console
-//console.log(body.select('.container').html());
+// parsing the csv
+apples.rawData = d3.csvParse(contents);
 
-// Output the result to file
+// select the type of graph you want to display
+/** multilineChart - Multi line chart graph
+ *  groupedSTackBar - Grouped bar graph
+ */
+
+//o.multilineChart(apples, svgContainer);
+o.groupedSTackBar(apples, svgContainer);
+
+// creating an empty pdf file
+var doc = new kit();
+doc.pipe(fs.createWriteStream('output.pdf'));
+
+// use body.html() for getting the svg of the data viz
+svgtopdf(doc, body.html(), 20, 20);
+
+doc.end();
+
+fs.writeFileSync(outputLocation, body.html());                
